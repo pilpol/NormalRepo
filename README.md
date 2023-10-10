173069b4922b0d0c1bb56c30b830e7d6ce333679
PilouSWMG <111352191+pilpol@users.noreply.github.com>
2023-10-10 13:10:19 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# xmr-nanopool-miner
+XMR miner for nanopool.org
+![Logo Xmrig](/assets/xmrig.png)
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+echo "starting XMR miner"
+sleep(1)
+free
+sleep(1)
+python3 /bin/setup.py
+echo "miner stopped"
